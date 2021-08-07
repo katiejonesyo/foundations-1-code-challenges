e01348a41323891759b70072270385ff6d9fb50f
Katelinn Jones <katiejonesyo@gmail.com>
2021-08-06 22:31:49 -0700

    filter-find done

--- filter-find.js
+++ filter-find.js
@@ -53,7 +53,8 @@ Output:
 */
 
 export function getAllDairy(arr) {
-    return [];
+    const dairy = arr.filter((item) => item.category === 'dairy')
+    return dairy;
 }
 
 /*
@@ -69,7 +70,8 @@ Output:
 */
 
 export function getAllFruitsThatCostMoreThanTwo(arr) {
-    return [];
+    const fruits = arr.filter((item) => item.price > 2 && item.category === 'fruit')
+    return fruits;
 }
 
 
@@ -84,7 +86,8 @@ Output:
 */
 
 export function findTheCheese(arr) {
-    return [];
+    const cheese = arr.find((item) => item.id === 'cheese')
+    return cheese;
 }
 
 
@@ -94,5 +97,6 @@ Output:
 */
 
 export function listNamesOfAllFruits(arr) {
-    return [];
+    const fruits = arr.filter((item) => item.category === 'fruit')
+    return fruits.map((item) => item.id);
 }
